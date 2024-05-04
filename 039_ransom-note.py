ad43d4b8947861e2512cedfd7e44525bbfe850a3
Jose Lo Huang <71886421+artneutro@users.noreply.github.com>
2024-05-04 17:52:02 +0100

    LeetCode - Ransom Note

---
+++ 039_ransom-note.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/ransom-note/
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        # Store the magazine values in a hashmap with the number of repetitions
+        valid_values = {}
+        index = 0
+        while index < len(magazine) :
+            if magazine[index] in valid_values :
+                valid_values[magazine[index]] += 1 
+            else :
+                valid_values[magazine[index]] = 1
+            index += 1
+        # Check the note characters and evaluate if they are in the magazine
+        index = 0
+        while index < len(ransomNote) :
+            if ransomNote[index] in valid_values :
+                if valid_values[ransomNote[index]] > 0 :
+                    valid_values[ransomNote[index]] -= 1
+                else :
+                    return False
+            else :
+                return False
+            index += 1
+        return True
+
+
