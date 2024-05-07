e6a005b567e5c29a10245085046d1ec7cb8e9a7a
Jose Lo Huang <71886421+artneutro@users.noreply.github.com>
2024-05-07 15:54:07 +0100

    LeetCode - Linked List Cycle

---
+++ 057_linked-list-cycle.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/linked-list-cycle/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = []
+        while (head) : 
+            # Store the object address (not the value), as there could be repeated values
+            visited.append(id(head)) 
+            if head.next : 
+                if id(head.next) in visited :
+                    return True
+            head = head.next 
+        return False
+
