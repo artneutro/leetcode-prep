8d1b28885d2e4bb942f5e921b153603df8bdffde
Jose Lo Huang <71886421+artneutro@users.noreply.github.com>
2024-05-12 10:58:40 +0100

    Bit Manipulation - Reverse Bits

---
+++ 126_reverse-bits.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/reverse-bits/
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        # Convert to binary 
+        # Remove the '0b' part
+        # Fill with leading '0' up to 32 bits
+        # Reverse
+        # Convert back to int
+        return int((str(bin(n))[2:]).zfill(32)[::-1], 2)
+
