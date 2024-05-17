2796da6e78183122ee0502a10b7bd022d6bf1326
Jose Lo Huang <71886421+artneutro@users.noreply.github.com>
2024-05-17 16:34:38 +0100

    Multidimensional DP - Triangle

---
+++ 142_triangle.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/triangle/
+class Solution:
+    def recursiveTotal(self, triangle: List[List[int]], cumulative: List[int]) -> int:
+        # If the triangle is finished return the minimum acummulative
+        if len(triangle) == 0 :
+            return min(cumulative)
+        # Else recursively get min sum of each next element with related cumulatives
+        else :
+            new_cumulative = []
+            index = 0
+            while index < len(triangle[-1]) :
+                new_min = min(triangle[-1][index]+cumulative[index], \
+                              triangle[-1][index]+cumulative[index+1])
+                new_cumulative.append(new_min)
+                index += 1
+            return self.recursiveTotal(triangle[:-1], new_cumulative)
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        return self.recursiveTotal(triangle[:-1], triangle[-1])
+
