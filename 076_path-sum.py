60525e19df3c6200bb0df009f68db5e3d71a8ac3
Jose Lo Huang <71886421+artneutro@users.noreply.github.com>
2024-06-05 17:06:19 +0100

    Binary Tree General - Path Sum

--- 076_path-sum.py
+++ 076_path-sum.py
@@ -6,17 +6,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def checkSum(self, root: Optional[TreeNode], targetSum: int, cumulative: List[int]) -> bool:
-    # Definition created to check once it reached a leaf if the cumulative sum is the same as the target
-        if root != None :
-            if root.left == None and root.right == None :
-                if cumulative + root.val == targetSum :
-                    return True
-            else :
-            # If is not a leaf then recursively send the cumulative to both children
-                return self.checkSum(root.left, targetSum, cumulative+root.val) \
-                or self.checkSum(root.right, targetSum, cumulative+root.val)
-        return False
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        return self.checkSum(root, targetSum, 0)
-
+        # Recursively substract the root.val from current targetSum 
+        if root != None :
+            # Once it reach the leaf check if the last substraction is zero
+            if root.left == None and root.right == None :
+                if targetSum - root.val == 0 :
+                    return True
+            # If is not a leaf then update the targetSum and send to children
+            else :
+                return self.hasPathSum(root.left, targetSum-root.val) \
+                or self.hasPathSum(root.right, targetSum-root.val)
+        return False
+        
