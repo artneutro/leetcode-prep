0b0339d57598dff3e5ddd0f2986b294473d128d2
Jose Lo Huang <71886421+artneutro@users.noreply.github.com>
2024-06-22 19:01:19 +0100

    Array / String - Best Time To Buy And Sell Stock II

--- 008_best-time-to-buy-and-sell-stock-ii.py
+++ 008_best-time-to-buy-and-sell-stock-ii.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # Base case
         if len(prices) == 1 :
             return 0
         max_profit = 0
