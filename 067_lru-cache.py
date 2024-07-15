bc6386e1c3570a91f19ff1dab4d50dbc7c79b0e2
Jose Lo Huang <71886421+artneutro@users.noreply.github.com>
2024-07-15 19:40:51 +0100

    Linked List - LRU Cache

---
+++ 067_lru-cache.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/lru-cache/
+class LRUCache:
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.priority = []
+
+    def get(self, key: int) -> int:
+        if key in self.cache :
+            self.priority.remove(key)
+            self.priority.append(key)
+            return self.cache[key]
+        else :
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        if key in self.priority :
+            self.priority.remove(key)
+            self.priority.append(key)
+        else :
+            self.priority.append(key)
+        if len(self.cache) > self.capacity :
+            key = self.priority.pop(0)
+            del self.cache[key]
+
