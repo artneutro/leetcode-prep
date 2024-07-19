e8ddc2215fe9ea06f10d24f2c12c1acdde55fafc
Jose Lo Huang <71886421+artneutro@users.noreply.github.com>
2024-07-19 19:54:41 +0100

    Array / String - Length Of Last Word

--- 019_length-of-last-word.py
+++ 019_length-of-last-word.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/length-of-last-word/
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        # Split the sentence and return the lenght of last word
         words = s.split()
         return len(words[-1])
 
